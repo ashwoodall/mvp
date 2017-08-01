660f940d932298642c42c601e81b606eda32184b
Ash Woodall <ash.woodall@gmail.com>
2017-08-01 11:01:32 -0500

    Add brewery information

--- src/components/Beer.jsx
+++ src/components/Beer.jsx
@@ -4,12 +4,16 @@ const Beer = ({ beer }) => (
   <div className="beer">
     <p><label>Name: </label> { beer.name }</p>
     { beer.labels && <img src={ beer.labels.medium } /> }
-    { !beer.labels && <img width='256' height='256' src='https://www.drinkpreneur.com/wp-content/uploads/2017/04/drinkpreneur_2016-01-26-1453821995-8643361-beermain.jpg' /> }
-    <p><label>Description: </label>{ beer.description }</p>
-    <p><label>Type: </label>{ beer.style.name }</p>
-    <p><label>ABV: </label> { beer.abv }</p>
-    <p><label>IBU: </label> { beer.ibu }</p>
+    { !beer.labels && <img width='256' height='auto' src='./assets/anonbeer.jpg' /> }
+
+
+    <p><label>Brewery: </label>{ beer.breweries[0].name === undefined ? ' Not Available' : ' ' + beer.breweries[0].name}</p>
+    <p><label>Description: </label>{ beer.description === undefined ? ' Not Available' : ' ' + beer.description}</p>
+    <p><label>Type: </label>{ beer.style.name === undefined ? ' Not Available' : ' ' + beer.style.name}</p>
+    <p><label>ABV: </label> { beer.abv === undefined ? ' Not Available' : ' ' + beer.abv}</p>
+    <p><label>IBU: </label> { beer.ibu === undefined ? ' Not Available' : ' ' + beer.ibu}</p>
   </div>
+
 );
 
 export default Beer;
